51f570d5956e9a9620465d1933a9a6212acf13fb
Chris Tufaro <ctufaro@gmail.com>
2019-01-30 10:41:21 -0500

    fixed untracked files

--- FinaroEngine.Functions/Function1.cs
+++ FinaroEngine.Functions/Function1.cs
@@ -1,35 +1,89 @@
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json;
 
 namespace FinaroEngine.Functions
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "simplechat", UserId = "ass")] SignalRConnectionInfo connectionInfo)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            return connectionInfo;
+        }
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+        [FunctionName("messages")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req,
+            [SignalR(HubName = "simplechat")]IAsyncCollector<SignalRMessage> signalRMessages)
+        {
 
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
+            var message = new JsonSerializer().Deserialize<ChatMessage>(new JsonTextReader(new StreamReader(req.Body)));
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    UserId = message.recipient,
+                    GroupName = message.groupname,
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+
+        [FunctionName("addToGroup")]
+        public static Task AddToGroup(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req,
+            [SignalR(HubName = "simplechat")]IAsyncCollector<SignalRGroupAction> signalRGroupActions)
+        {
+
+            var message = new JsonSerializer().Deserialize<ChatMessage>(new JsonTextReader(new StreamReader(req.Body)));
+
+
+            return signalRGroupActions.AddAsync(
+                new SignalRGroupAction
+                {
+                    UserId = message.recipient,
+                    GroupName = message.groupname,
+                    Action = GroupAction.Add
+                });
+        }
+
+        [FunctionName("removeFromGroup")]
+        public static Task RemoveFromGroup(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req,
+            [SignalR(HubName = "simplechat")]IAsyncCollector<SignalRGroupAction> signalRGroupActions)
+        {
+
+            var message = new JsonSerializer().Deserialize<ChatMessage>(new JsonTextReader(new StreamReader(req.Body)));
+
+
+            return signalRGroupActions.AddAsync(
+                new SignalRGroupAction
+                {
+                    UserId = message.recipient,
+                    GroupName = message.groupname,
+                    Action = GroupAction.Remove
+                });
+        }
+
+        public class ChatMessage
+        {
+            public string sender { get; set; }
+            public string text { get; set; }
+            public string groupname { get; set; }
+            public string recipient { get; set; }
+            public bool isPrivate { get; set; }
         }
     }
 }
