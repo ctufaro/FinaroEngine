8aabc702784861c0e5c823f05801c70936b7f6ca
Chris Tufaro <ctufaro@gmail.com>
2019-04-09 10:12:15 -0400

    Cleanup

--- FinaroEngine.SMS/Program.cs
+++ FinaroEngine.SMS/Program.cs
@@ -10,30 +10,20 @@ namespace FinaroEngine.SMS
     {
         static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
-
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
             var configuration = builder.Build();
-
-            string myKey1 = configuration["myKey1"];
-            Console.WriteLine(myKey1);
-
-            string foo = configuration.GetSection("foo").Value;
-            Console.WriteLine(foo);
-
 
             // Find your Account Sid and Token at twilio.com/console
             // DANGER! This is insecure. See http://twil.io/secure
-            const string accountSid = "";
-            const string authToken = "";
+            string accountSid = configuration["AcctSID"];
+            string authToken = configuration["AuthToken"];
 
             TwilioClient.Init(accountSid, authToken);
 
             var message = MessageResource.Create(
                 body: "Welcome to the S'Way Exchange!\n\rNEW YORK GIANTS trading @ .70 SWAY Tokens\n\rText 1 to go long 10 Units.",
                 from: new Twilio.Types.PhoneNumber("+19177463774"),
-                to: new Twilio.Types.PhoneNumber("+1646")
+                to: new Twilio.Types.PhoneNumber("+16463153954")
             );
 
             Console.WriteLine(message.Sid);
