31f2bb6d2b54d62ca4cfb6ce1453e9a7a12f0b7b
christufaro <ctufaro@gmail.com>
2019-12-13 12:05:08 -0500

    Select all

--- FinaroEngine.Functions/VueFx.cs
+++ FinaroEngine.Functions/VueFx.cs
@@ -8,13 +8,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using FinaroEngine.Library.Domain;
+using System.Collections.Generic;
 
 namespace FinaroEngine.Functions
 {
     public static class VueFx
     {
         [FunctionName("insertregister")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> InsertRegister(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -23,6 +24,18 @@ namespace FinaroEngine.Functions
             Register data = JsonConvert.DeserializeObject<Register>(requestBody);
             Register register = new Register(Environment.GetEnvironmentVariable("SQLConnectionString"));
             await register.InsertRegisterAsync(data);
+            return new OkObjectResult(data);
+        }
+
+        [FunctionName("selectregister")]
+        public static async Task<IActionResult> SelectRegister(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Register register = new Register(Environment.GetEnvironmentVariable("SQLConnectionString"));
+            List<Register> data = await register.SelectRegisterAsync();
             return new OkObjectResult(data);
         }
     }
--- FinaroEngine.Library/Domain/Register.cs
+++ FinaroEngine.Library/Domain/Register.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,11 @@ namespace FinaroEngine.Library.Domain
         public int SchoolDay { get; set; }
         public DateTime CreatedOn { get; set; }
 
+        public Register()
+        {
+
+        }
+
         public Register(string connection)
         {
             this._connection = connection;
@@ -50,6 +56,36 @@ namespace FinaroEngine.Library.Domain
             parameters.Add(new SqlParameter("@SCHOOLDAY", register.SchoolDay));
             await DBUtility.ExecuteQueryAsync(this._connection, "spInsertRegister", parameters);
             return true;
+        }
+
+        public async Task<List<Register>> SelectRegisterAsync()
+        {
+            DataTable dt = await DBUtility.GetDataTableAsync(_connection, "spSelectRegister", null);
+            List<Register> registers = new List<Register>();
+            foreach(DataRow dr in dt.Rows)
+            {
+                Register r = new Register()
+                {
+                    ActivitySheetSubject = dr["ActivitySheetSubject"].ToString(),
+                    Address = dr["Address"].ToString(),
+                    CreatedOn = Convert.ToDateTime(dr["CreatedOn"]),
+                    EmailPrimary = dr["EmailPrimary"].ToString(),
+                    EmailSecondary = dr["EmailSecondary"].ToString(),
+                    FirstName = dr["FirstName"].ToString(),
+                    Id = Convert.ToInt32(dr["Id"]),
+                    LastName = dr["LastName"].ToString(),
+                    Position = Convert.ToInt32(dr["Position"]),
+                    RegisterCode = dr["RegisterCode"].ToString(),
+                    RegisterId = dr["RegisterId"].ToString(),
+                    SchoolDay = Convert.ToInt32(dr["SchoolDay"]),
+                    SchoolName = dr["SchoolName"].ToString(),
+                    Street = dr["Street"].ToString(),
+                    Town = dr["Town"].ToString(),
+                    Zip = dr["Zip"].ToString()
+                };
+                registers.Add(r);
+            }
+            return registers;
         }
     }
 }
